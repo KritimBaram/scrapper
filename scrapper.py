c54b07bfe6565d2e96ef11392e56455c36b1bc66
Kritim Baram <kritimbaram144@gmail.com>
2025-05-08 17:52:24 +0545

    Organize imports

--- scrapper.py
+++ scrapper.py
@@ -3,10 +3,11 @@
 #  python -m pip install requests
 # => parse html
 
-
+import json
 import requests 
-from bs4 import BeautifulSoup
 import sqlite3
+
+from bs4 import BeautifulSoup
 
 URL = "https://books.toscrape.com/"
 def create_table():
@@ -71,8 +72,7 @@ def scrape_books(url):
     return books
 
 def save_to_json(books):
-     import json
-
+   
      with open("books.json", "w", encoding = "utf-8") as f:
           json.dump(books,f, indent = 4, ensure_ascii=False)
 create_table()
